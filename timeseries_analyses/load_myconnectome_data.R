4a952a3f620ea5a03d2d8725b623fac38322d439
poldrack <poldrack@gmail.com>
2014-11-01 13:34:53 -0700

    initial add

---
+++ timeseries_analyses/load_myconnectome_data.R
@@ -0,0 +1,271 @@
+# these functions load the data used for the MyConnectome study
+
+
+test_load_myconnectome_data = function() {
+	# test loading of all data
+	# check sums of numeric variables, allowing for some rounding error
+	
+	print("This tool requires installation of the R.cache package")
+	
+	require(R.cache)
+	
+	print("testing all variables against stored checksums, this will take a few moments...")
+	print("you can safely ignore warnings about NAs induced by coercion...")
+	
+	behav=load_behav_data()
+	stopifnot(getChecksum(behav)=="1c26494a2a3da1ac3e00f6a5704b03d4")
+
+	diary_data=load_diary_data()
+	stopifnot(getChecksum(diary_data)=="2a04da4ac8a0fc6e422e155590a5fe09")
+	
+	rnaseq_data=load_rnaseq_data()
+	stopifnot(getChecksum(rnaseq_data)=="63e9dd4967ed3fffdd0ef059d01c11ef")
+	
+	immport=load_ImmPort_data()
+	stopifnot(getChecksum(immport)=="df979f524c61337058adbff3bdd03d3e")
+	
+	metab=load_metab_data()
+	stopifnot( getChecksum(metab) == "9843157712fa6c7175fcfa74f0b5585d")
+	
+	food=load_food_data()
+	stopifnot(getChecksum(food) == "57ecfbdaf43cb586bf653345e18cd28d")
+	
+	fd=load_fd_data()
+	stopifnot(getChecksum(fd) == "bc5139aa411e9d2cb7d649f5b01af985")
+	
+	wincorr=load_fmri_data('wincorr')
+	stopifnot(getChecksum(wincorr) == "29bc5e23e47b46285900a2a1909a8cff")
+
+	winvar=load_fmri_data('winvar')
+	stopifnot(getChecksum(winvar) == "a82638328dbef2621d63cbc8dd99f3bb")
+	
+	falff=load_fmri_data('falff')
+	stopifnot(getChecksum(falff) == "e3bd0d1c8acb8404d804d5bc73c5d978")
+	
+	netdat=load_network_data()
+	stopifnot(getChecksum(netdat)=="500bb83ce3bd9e9f30dcfc596f6790c1")
+	
+	print('All checksums confirmed')
+	
+	}
+	
+load_behav_data = function (infile="http://s3.amazonaws.com/openfmri/ds031/behavior/trackingdata.txt") {
+	
+	# load behav data
+	behav = read.table(infile, na.strings = '.', header=TRUE)
+	
+	
+	# take mean temp
+	behav$"temp.mean"=(behav$'weather.temphi'+behav$'weather.templo')/2.0
+	
+	behav$day_of_week2 = weekdays(as.Date(behav$date))
+	# Create indicator that where tu=0 and th = 1
+	behav$tu_th = rep(NA, length(behav$day_of_week2))
+	behav$tu_th[behav$day_of_week2 == "Tuesday"] = 0
+	behav$tu_th[behav$day_of_week2 == "Thursday"] = 1
+	# take mean temp
+	behav$"temp.mean"=(behav$'weather.temphi'+behav$'weather.templo')/2.0
+	
+	behav$prevevening.Timespentoutdoors = as.numeric(as.character(behav$prevevening.Timespentoutdoors))
+	
+	behav$date=as.Date(behav$date)
+	return(behav)
+
+}
+
+load_diary_data=function(infile='http://s3.amazonaws.com/openfmri/ds031/diary/term_date.txt',goodvars=c('allergy','exercise')) {
+	termdata=read.table(infile,header=FALSE)
+	dates=read.table('http://s3.amazonaws.com/openfmri/ds031/diary/diary_dates.txt')$V1
+	terms=as.character(read.table('http://s3.amazonaws.com/openfmri/ds031/diary/diary_terms.txt')$V1)
+	names(termdata)=terms
+	termdata=subset(termdata,select=goodvars)
+	termdata$date=as.Date(dates)
+	# remove obvious useless terms
+	return(termdata)
+	
+	}
+	
+	
+load_rnaseq_data = function(use_ME=TRUE,limit_ME_to_enriched=TRUE,scale=FALSE,varstab_file='http://s3.amazonaws.com/openfmri/ds031/RNA-seq/varstab_data_rinregressed.txt',me_file='http://s3.amazonaws.com/openfmri/ds031/RNA-seq/MEs-thr8-rinreg-48sess.txt',datefile='http://s3.amazonaws.com/openfmri/ds031/RNA-seq/drawdates.txt') {
+	rnaseq.dat.full= read.table(varstab_file, na.strings='.', header=TRUE)
+	rna_subcodes=names(rnaseq.dat.full)
+	rna_labels=row.names(rnaseq.dat.full)
+	
+	rnaseq.dat.me = read.table(me_file, na.strings='.', header=TRUE)
+	row.names(rnaseq.dat.me)=rna_subcodes
+	# these are the modules that are not enriched for at least one term at benjamini p < 0.5
+	unenriched=c('ME8','ME9','ME15','ME16','ME17','ME18','ME20','ME23','ME24','ME25','ME26','ME28','ME31','ME32','ME36','ME38')
+	MEsubset=c()
+	for (i in 1:38) {
+		MEname=sprintf('ME%d',i)
+		if (limit_ME_to_enriched)  {
+			if (!(MEname %in% unenriched)) {
+				MEsubset=c(MEsubset,MEname)
+				}
+			} else {
+				MEsubset=c(MEsubset,MEname)
+		
+				}
+		}
+	rnaseq.dat.me=subset(rnaseq.dat.me,select=MEsubset)
+	
+	if (use_ME) {
+		rnaseq.dat=rnaseq.dat.me
+		}  else {
+		rnaseq.dat=as.data.frame(t(rnaseq.dat.full))
+		}
+	
+	rna_labels=names(rnaseq.dat)
+	if (scale) {
+		rnaseq.dat=as.data.frame(scale(rnaseq.dat,scale=FALSE))
+		names(rnaseq.dat)=rna_labels
+	}
+	# scale removes labels, so put them back
+	d=read.table(datefile)
+	rnaseq.dat$date=as.Date(d$V1,'%m/%d/%Y')
+	return(rnaseq.dat)
+
+}
+	
+load_ImmPort_data = function(datefile='http://s3.amazonaws.com/openfmri/ds031/RNA-seq/drawdates.txt',infile='http://s3.amazonaws.com/openfmri/ds031/RNA-seq/ImmPort_eigengenes_rinregressed.txt') {
+	clusterdata.dat= as.data.frame(t(read.table(infile, na.strings='.', header=FALSE,row.names=1)))
+
+	d=read.table(datefile)
+	clusterdata.dat$date=as.Date(d$V1,'%m/%d/%Y')
+	return(clusterdata.dat)
+	}
+
+load_metab_data = function(use_clustered_data=TRUE,clust_file='http://s3.amazonaws.com/openfmri/ds031/metabolomics/apclust_eigenconcentrations.txt',exclude_unenriched=TRUE,clust_desc_file='http://s3.amazonaws.com/openfmri/ds031/metabolomics/apclust_descriptions.txt',logtransform=TRUE,exclude_unnamed=TRUE,infile='http://s3.amazonaws.com/openfmri/ds031/metabolomics/metabolomics.txt',labelfile='http://s3.amazonaws.com/openfmri/ds031/metabolomics/metabolomics_labels.txt',datefile='http://s3.amazonaws.com/openfmri/ds031/RNA-seq/drawdates.txt',subcodes='http://s3.amazonaws.com/openfmri/ds031/RNA-seq/pathsubs.txt') {
+	
+	if (use_clustered_data) {
+		labels=read.table(clust_desc_file,header=FALSE,sep='\t')$V1
+		metab.dat=read.table(clust_file)
+		names(metab.dat)=as.character(labels)
+		if (exclude_unenriched) {
+			# exclude those with FDR p<0.1
+			metab.dat=subset(metab.dat,select=-c(1,3,7,8,13,15,16))
+			}
+		} else {
+		metab.dat=read.table(infile, na.strings='.', header=FALSE)
+		metab.labels=read.table(labelfile, sep='\t',na.strings='.', header=FALSE)
+		metab.labels=as.character(metab.labels$V1)
+		for (i in 1:length(metab.labels)) {
+			metab.labels[i]=gsub(' ','_',metab.labels[i])
+			}
+		if (exclude_unnamed) {
+			metab.dat=metab.dat[,1:106]
+			}
+		if (logtransform) {# NB: log-transform the metabolomics data
+			metab.dat=log(metab.dat)
+			}
+		if (exclude_unnamed) {
+			names(metab.dat)=as.character(metab.labels[1:106])
+			} else {
+			names(metab.dat)=as.character(metab.labels)
+		}
+	}
+	
+	d=read.table(datefile)
+	metab.dat$date=as.Date(d$V1,'%m/%d/%Y')
+
+	row.names(metab.dat)=read.table(subcodes)$V1
+
+	return(metab.dat)	
+	}
+
+
+load_food_data = function() {
+	# laod behav data to get dates
+	behav=load_behav_data()
+	
+	food = read.table("http://s3.amazonaws.com/openfmri/ds031/behavior/food_data.txt", na.strings = '.', header=TRUE)
+	food$date=behav$date[behav$subcode %in% food$subcode]
+
+	# combine olive oil and vinegar as they are perfectly correlated
+	food$olive_oil_and_vinegar=behav$olive_oil
+	food$olive_oil=NULL
+	food$vinegar=NULL
+	return(food)
+	}
+	
+load_fd_data=function() {
+	behav=load_behav_data()
+	subcodes=read.table("http://s3.amazonaws.com/openfmri/ds031/rsfmri/subcodes.txt",header=FALSE)
+	dates=behav$date[behav$subcode %in% subcodes$V1]
+	fd=read.table('http://s3.amazonaws.com/openfmri/ds031/rsfmri/mean_fd.txt',header=FALSE)
+	fd$date=dates
+	return(fd)
+	
+	}
+	
+load_autocorr_data = function(infile='/Users/poldrack/Dropbox/data/selftracking/rsfmri/autocorr_data.txt') {
+	# laod behav data to get dates
+	behav=load_behav_data()
+	subcodes=read.table("http://s3.amazonaws.com/openfmri/ds031/rsfmri/subcodes.txt",header=FALSE)
+	dates=behav$date[behav$subcode %in% subcodes$V1]
+	data = read.table(infile)
+	data$date=dates
+	return(data)
+	}
+	
+load_fmri_data = function (type='wincorr',log_winvar=TRUE) {
+	
+# laod behav data to get dates
+behav=load_behav_data()
+
+subcodes=read.table("http://s3.amazonaws.com/openfmri/ds031/rsfmri/subcodes.txt",header=FALSE)
+dates=behav$date[behav$subcode %in% subcodes$V1]
+
+if (type=='wincorr') {
+	fmridatafile="http://s3.amazonaws.com/openfmri/ds031/rsfmri/module_within_corr.txt"
+	network_names=c('1_Default','2_Second_Visual','3_Frontal-Parietal','4.5_First_Visual_V1plus','5_First_Dorsal_Attention','6_Second_Dorsal_Attention','7_Ventral_Attention-Language','8_Salience','9_Cingulo-opercular','10_Somatomotor','11.5_Frontal-Parietal_Other','15_Parietal_Episodic_Retrieval','16_Parieto-Occipital')
+  } else if (type == 'winvar') {
+  	fmridatafile="http://s3.amazonaws.com/openfmri/ds031/rsfmri/network_variance_allses_fixed.txt"
+  	network_names=c('1_Default','2_Second_Visual','3_Frontal-Parietal','4.5_First_Visual_V1plus','5_First_Dorsal_Attention','6_Second_Dorsal_Attention','7_Ventral_Attention-Language','8_Salience','9_Cingulo-opercular','10_Somatomotor','11.5_Frontal-Parietal_Other','15_Parietal_Episodic_Retrieval','16_Parieto-Occipital')
+  	} else {
+	fmridatafile="http://s3.amazonaws.com/openfmri/ds031/rsfmri/module_falff.txt"
+	network_names=c('1_Default','2_Second_Visual','3_Frontal-Parietal','4.5_First_Visual_V1plus','5_First_Dorsal_Attention','6_Second_Dorsal_Attention','7_Ventral_Attention-Language','8_Salience','9_Cingulo-opercular','10_Somatomotor','11.5_Frontal-Parietal_Other','15_Parietal_Episodic_Retrieval','16_Parieto-Occipital')
+	} 		
+
+data = read.table(fmridatafile, na.strings='.', header=FALSE)
+if (type == 'winvar' & log_winvar==TRUE) {
+	data=log(data)
+}
+
+names(data)=network_names
+data$date=dates
+
+	return(data)
+	}
+	
+load_network_data=function(mean_over_thresh=FALSE) {
+	behav=load_behav_data()
+	data=read.table('http://s3.amazonaws.com/openfmri/ds031/rsfmri/netstats_all.txt',header=TRUE)
+	if (mean_over_thresh) {
+		vars=c('EFFg','EFFl','Clust','modularity_infomap',
+		  "modularity_multi","PowerExp","RCC",
+		  "mean_cc","mean_bc","mean_pi","gcsize","APL")
+		  meandata=matrix(data=0,nrow=dim(data)[1],ncol=length(vars))
+		  for (measure in 1:length(vars)) {
+				d=subset(data,select=seq(measure,60,12))
+				meandata[,measure]=apply(d,1,mean)
+			
+			}
+		data=as.data.frame(meandata)
+		names(data)=vars
+		data$MOD=(data$modularity_infomap + data$modularity_multi)/2.0
+		data$modularity_infomap=NULL
+		data$modularity_multi=NULL
+		data$mean_pi=NULL
+		data$mean_cc=NULL
+		data$mean_bc=NULL
+		data$gcsize=NULL
+		}
+	subcodes=read.table("http://s3.amazonaws.com/openfmri/ds031/rsfmri/subcodes.txt",header=FALSE)
+	dates=behav$date[behav$subcode %in% subcodes$V1]
+	row.names(data)=subcodes$V1
+	data$date=dates
+	return(data)
+	
+
+	}
---
+++ timeseries_analyses/load_myconnectome_data.py
@@ -0,0 +1,152 @@
+import numpy
+import os
+
+def get_diary_data():
+    basedir='/Users/poldrack/code/selftracking/analysis_metadata'
+    
+def get_matching_datasets(d1,d2,s1,s2):
+    """
+    get_matching_datasets(d1,d2,s1,s2)
+    """
+    
+    joint_subs=set(s1).intersection(s2)
+    s1_idx=[i for i in range(len(s1)) if s1[i] in joint_subs]
+    s2_idx=[i for i in range(len(s2)) if s2[i] in joint_subs]
+    for i in range(len(s1_idx)):
+        assert s1[s1_idx[i]]==s2[s2_idx[i]]
+    d1_joint=d1[s1_idx,:]
+    d2_joint=d2[s2_idx,:]
+    return d1_joint, d2_joint,joint_subs
+
+def load_immport_data():
+    f=open('/Users/poldrack/Dropbox/data/selftracking/rna-seq/ImmPort/ImmPort_eigengenes_rinregressed.txt')
+    lines=[]
+    gene_names=[]
+    for l in f.readlines():
+            l_s=l.strip().split()
+            gene_names.append(l_s[0])
+            lines.append([float(l_s[i]) for i in range(1,len(l_s))])
+    f.close()
+    data=numpy.array(lines).T
+    subcodes=[i.strip() for i in open('/Users/poldrack/Dropbox/data/selftracking/rna-seq/pathsubs.txt').readlines()]
+    return data,gene_names,subcodes
+    
+    
+def load_rnaseq_data(use_wgcna=True):
+    if use_wgcna:
+        f=open('/Users/poldrack/Dropbox/data/selftracking/rna-seq/WGCNA/MEs-thr8-rinreg-48sess.txt')
+        header=[i.replace('"','') for i in f.readline().strip().split()]
+        MEs=[int(i.replace('ME','')) for i in header]
+        sort_idx=numpy.argsort(MEs)
+        gene_names=[MEs[i] for i in sort_idx]
+        lines=[]
+        for l in f.readlines():
+            l_s=l.strip().split()
+            lines.append([float(l_s[i]) for i in sort_idx])
+        f.close()
+        data=numpy.array(lines)
+        subcodes=[i.strip() for i in open('/Users/poldrack/Dropbox/data/selftracking/rna-seq/pathsubs.txt').readlines()]
+    else:  # use full data
+        f=open('/Users/poldrack/Dropbox/data/selftracking/rna-seq/varstab_data_rinregressed.txt')   
+        subcodes=[i.replace('"','') for i in f.readline().strip().split(' ')]
+        
+        lines=[]
+        gene_names=[]
+        for line in f.readlines():
+            l_s=line.strip().split(' ')
+            lines.append([float(i) for i in l_s[1:]])
+            gene_names.append(l_s[0].replace('"',''))
+        f.close()
+        data=numpy.array(lines).T
+    f=open('/Users/poldrack/Dropbox/data/selftracking/rna-seq/drawdates.txt')  
+    dates=[i for i in f.readline().split('\r')]
+    f.close()
+    return data,gene_names,dates,subcodes
+
+def load_behav_data(subcodes_limit=None,xvars=None):
+    """
+    load behavioral data - pass dates to limit to specific subcodes
+    """
+    
+    f=open('/Users/poldrack/code/selftracking/analysis_metadata/trackingdata_goodscans.txt')
+    header=[i for i in f.readline().strip().split('\t')]
+    variables=header[2:]  # remove 
+    lines=[]
+    for line in f.readlines():
+        l_s=line.strip().split('\t')
+        if subcodes_limit:
+            if not set(subcodes_limit).intersection([l_s[0]]):
+                continue
+        lines.append(l_s)
+
+    f.close()
+    
+    behavdata=numpy.zeros((len(lines),len(variables)))
+    dates=[]
+    subcodes=[]
+    for i in range(len(lines)):
+        line=lines[i]
+        for v in range(len(variables)):
+            try:
+                behavdata[i,v]=float(line[v+2])
+            except:
+                behavdata[i,v]=numpy.nan
+        dates.append(line[1])
+        subcodes.append(line[0])
+    if subcodes_limit:
+        try:
+            assert len(subcodes)==len(subcodes_limit)
+        except:
+            print 'there is a problem: subcodes do not match subcodes_limit'
+    if xvars:
+        xvar_nums=[]
+        for name in xvars:
+            varnum=[i for i in range(len(variables)) if variables[i]==name.replace('.',':')]
+            xvar_nums.append(varnum[0])
+        behavdata=behavdata[:,xvar_nums]
+        variables=[variables[i] for i in xvar_nums]
+    
+    return behavdata,variables,dates,subcodes
+
+def load_wincorr_data():
+    wincorr=numpy.loadtxt('/Users/poldrack/Dropbox/data/selftracking/rsfmri/module_within_corr.txt')
+    netnames=['1_Default','2_Second_Visual','3_Frontal-Parietal','4.5_First_Visual_V1plus',
+              '5_First_Dorsal_Attention','6_Second_Dorsal_Attention','7_Ventral_Attention-Language',
+              '8_Salience','9_Cingulo-opercular','10_Somatomotor','11.5_Frontal-Parietal_Other',
+              '15_Parietal_Episodic_Retrieval','16_Parieto-Occipital']
+    
+    subcodes=[i.strip() for i in open('/Users/poldrack/Dropbox/data/selftracking/rsfmri/subcodes.txt').readlines()]
+    return wincorr,netnames,subcodes
+    
+def load_fullcorr_data():
+    fullcorr=numpy.load('/Users/poldrack/Dropbox/data/selftracking/rsfmri/corrdata.npy')
+    subcodes=[i.strip() for i in open('/Users/poldrack/Dropbox/data/selftracking/rsfmri/subcodes.txt').readlines()]
+
+    return fullcorr,subcodes
+
+def load_partialcorr_data(cross_thresh=False,thresh=0.05):
+    thresh_dict={0.01:0,0.025:1,0.05:2,0.075:3,0.1:4}
+    
+    partialcorr=numpy.load('/Users/poldrack/Dropbox/data/selftracking/rsfmri/huge_adj.npy')
+    subcodes=[i.strip() for i in open('/Users/poldrack/Dropbox/data/selftracking/rsfmri/subcodes.txt').readlines()]
+    if not cross_thresh:
+        pc=partialcorr[:,thresh_dict[thresh],:]
+        pc=pc.squeeze()
+    else:
+        pc=numpy.sum(partialcorr,1)
+    v=numpy.var(pc,0)
+    pc_good=pc[:,v>0]
+    return pc_good,subcodes
+
+def load_illness_data():
+    f=open('/Users/poldrack/code/selftracking/analysis_metadata/health_and_drawdates.txt')
+    lines=[i.strip().split() for i in f.readlines()]
+    f.close()
+    illness=[]
+    for l in lines:
+        if not l[1]=='none':
+            illness.append(1)
+        else:
+            illness.append(0)
+    illness=numpy.array(illness)
+    return illness
