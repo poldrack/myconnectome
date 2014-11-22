3b99ccde7482d12bd88d42ff25aa221aba7ab303
poldrack <poldrack@gmail.com>
2014-11-22 12:53:53 -0800

    added metab, fixed parsing of files

--- timeseries_analyses/mk_graph.py
+++ timeseries_analyses/mk_graph.py
@@ -6,13 +6,18 @@ import pydot
 import networkx as nx
 import re
 import os,glob
+import numpy
+
 
 filter_negatives=False
 exclude_metab=False
-exclude_metab_metab=True
+exclude_metab_metab=False
 exclude_gene_gene=True
 filter_gene_modules=False # only include first cluster in each module
 thresh=0.1
+degree_thresh=1
+
+exclude_unenriched=True
 
 def load_dataframe(filename,thresh=0.1):
 	# return p value, t stat, and correlation
@@ -22,7 +27,20 @@ def load_dataframe(filename,thresh=0.1):
 	f.close()
 	data={}
 	for l in lines:
-		l_s=[i.replace('"','') for i in l.strip().split()]
+		l_fixed=[]
+		in_quotes=False
+		for i in range(len(l)):
+			if l[i]=='"':
+				if in_quotes:
+					in_quotes=False
+				else:
+					in_quotes=True
+			if l[i]==' ' and not in_quotes:
+				l_fixed.append('\t')
+			else:
+				l_fixed.append(l[i])
+		l_fixed=''.join(l_fixed).replace('"','')
+		l_s=[i.replace('"','') for i in l_fixed.strip().split('\t')]
 		try:
 		 if float(l_s[-1])<thresh:
 			#print l_s
@@ -36,7 +54,13 @@ cluster_terms=[' '.join(i.strip().split()[1:]) for i in open('/Users/poldrack/Dr
 cluster_dict={}
 for i in range(len(cluster_names)):
 	cluster_dict[cluster_names[i]]=cluster_terms[i]
-	
+
+metab_names=['C%d'%i for i in range(1,16)]
+metab_terms=[i.strip() for i in open('/Users/poldrack/Dropbox/data/selftracking/proteomics/apclust_descriptions.txt').readlines()]
+metab_dict={}
+for i in range(len(metab_names)):
+	metab_dict[metab_names[i]]=metab_terms[i]
+
 files_to_load=list(set(glob.glob('/Users/poldrack/Dropbox/data/selftracking/timeseries_analyses/out*.txt')))
 
 files_to_load=['/Users/poldrack/Dropbox/data/selftracking/timeseries_analyses/out.dat.wgcna_wincorr.txt',
@@ -44,13 +68,14 @@ files_to_load=['/Users/poldrack/Dropbox/data/selftracking/timeseries_analyses/ou
 			'/Users/poldrack/Dropbox/data/selftracking/timeseries_analyses/out.dat.wgcna_wgcna.txt',
 			'/Users/poldrack/Dropbox/data/selftracking/timeseries_analyses/out.dat.wgcna_behav.txt',
 			'/Users/poldrack/Dropbox/data/selftracking/timeseries_analyses/out.dat.wincorr_behav.txt',
-			'/Users/poldrack/Dropbox/data/selftracking/timeseries_analyses/out.dat.behav_behav.txt']
+			'/Users/poldrack/Dropbox/data/selftracking/timeseries_analyses/out.dat.behav_behav.txt',
+			'/Users/poldrack/Dropbox/data/selftracking/timeseries_analyses/out.dat.metab_wincorr.txt',
+			'/Users/poldrack/Dropbox/data/selftracking/timeseries_analyses/out.dat.behav_metab.txt',
+			'/Users/poldrack/Dropbox/data/selftracking/timeseries_analyses/out.dat.wgcna_metab.txt']
 #files_to_load=['metab_metab']
 
 #power_network_names={-1:'none',0:'none',1:'DM',2:'Vis2',3:'FP',4.5:'Vis1',5:'DA1',6:'DA2',7:'VA-Lang',8:'Sal',9:'CO',10:'SOM',11.5:'FPother',15:'PEpisRet',16:'PO'}
 power_network_names={-1:'none',0:'none',1:'DefaultMode',2:'Visual-II',3:'Fronto-parietal',4.5:'Visual-I',5:'DorsalAttn-I',6:'DorsalAttn-II',7:'VentralAttn/Lang',8:'Salience',9:'Cingulo-opercular',10:'Somatomotor',11.5:'OtherFP',15:'Parietal EpisRet',16:'Parieto-occipital'}
-netdat_full_names={'EFFg':'global efficiency','EFFl':'local efficiency','APL':'average path length',
-				'PowerExp':'Power exponent','Clust':'clustering coeff','MOD':'Modularity','RCC':'Rich club coefficient'}
 node_shapes={'metab':'box','wgcna':'ellipse','food':'triangle','wincorr':'diamond','behav':'hexagon','falff':'invtriangle','netdat':'trapezium'}
 node_classes={'metab':1,'wgcna':2,'food':3,'wincorr':4,'behav':5,'falff':6,'netdat':7}
 
@@ -59,8 +84,8 @@ behav_terms={'panas.positive':'Positive mood','panas.negative':'Negative mood','
              'morning.Sleepquality':'Self-rated sleep quality','morning.Soreness':'Soreness','prevevening.Alcohol':'Alcohol intake (previous evening)',
              'prevevening.Guthealth':'Gut health (previous day)','prevevening.Psoriasisseverity':'Psoriasis severity (previous day)',
              'prevevening.Stress':'Stress (previous day)', 'prevevening.Timespentoutdoors':'Time spent outdoors (previous day)',
-             'tu_th':'Thursday vs. Tuesday', 'temp.mean':'Mean daily temp',"email.LIWC_CDI":'Email content-dynamic index',
-             "email.LIWC_negemo":'Email negative emotion',"email.LIWC_posemo":'Email positive emotion','zeo.zq':'ZEO zq'}
+             'TuesThurs':'Thursday vs. Tuesday', 'temp.mean':'Mean daily temp',"email.LIWCcdi":'Email content-dynamic index',
+             "email.LIWCnegemo":'Email negative emotion',"email.LIWCposemo":'Email positive emotion','zeo.zq':'ZEO zq'}
 
 data={}
 graph = nx.Graph()
@@ -95,12 +120,6 @@ for filename in files_to_load:
 				dt[1]=True
 		
 		k=[i.replace(',','').replace('"','') for i in list(k)]
-		#print k
-		#print k
-		## if datatypes[0] in ['wincorr','falff']:
-		## 	k[0]=power_network_names[float(k[0])]
-		## if datatypes[1] in ['wincorr','falff']:
-		## 	k[1]=power_network_names[float(k[1])]
 		
 		nodenames=[datatypes[0]+'-'+k[0],datatypes[1]+'-'+k[1]]
 		badlevel=False
@@ -110,19 +129,20 @@ for filename in files_to_load:
 					badlevel=True
 		if badlevel:
 			continue
+		exclude=False
 		for x in range(2):
 			name=u'%s'%re.sub(r'[^\x00-\x7F]+',' ', nodenames[x]).replace('"','').replace('&','')
-			nodelabel=''.join(name.split('-')[1:]).replace('"','').replace('_NIST','')
+			if name.find('no enrichment')>-1 and exclude_unenriched:
+				exclude=True
+			nodelabel=''.join(name.split('-')[1:]).replace('"','').replace('_NIST','').split(':')[0]
+			print name,nodelabel
 			if datatypes[x]=='wincorr':
-				nodelabel=''.join(nodelabel.split('_')[1:])
-				print nodelabel
-			if datatypes[x]=='falff':
-				modnum=float(nodelabel.split('_')[0])
-				nodelabel=power_network_names[modnum]+'-falff'
+				nodelabel=power_network_names[float(nodelabel)]
+				print 'wincorr:',nodelabel
 			if datatypes[x]=='wgcna':
 				nodelabel=cluster_dict[nodelabel]+' ('+nodelabel.replace('mod','').replace('clust','')+')'
-			if datatypes[x]=='netdat':
-				nodelabel=netdat_full_names[nodelabel]
+			if datatypes[x]=='metab':
+				nodelabel=metab_dict[nodelabel]+' ('+nodelabel+')'
 			if datatypes[x]=='behav':
 				nodelabel=behav_terms[nodelabel].replace(' (previous evening)','').replace(' (previous day)','')
 				
@@ -133,8 +153,9 @@ for filename in files_to_load:
 				print name,graph.node[name]
 				shell[node_classes[datatypes[x]]-1].append(name)
 			nodenames[x]=name
-		graph.add_edge(nodenames[0],nodenames[1],attr_dict={'pval':data[f][ktuple][0],'tval':data[f][ktuple][1],'rval':data[f][ktuple][2]})
-		print 'edge:',nodenames[0],nodenames[1]
+		if not exclude:
+			graph.add_edge(nodenames[0],nodenames[1],attr_dict={'pval':data[f][ktuple][0],'tval':data[f][ktuple][1],'rval':data[f][ktuple][2]})
+			print 'edge:',nodenames[0],nodenames[1]
 
 
 sg=nx.connected_component_subgraphs(graph)
@@ -168,6 +189,12 @@ else:
 	filt=''
 if exclude_metab:
 	filt=filt+'_nometab'
+if degree_thresh>1:
+	filt=filt+'degree%d'%degree_thresh
+	for n in graph.nodes():
+		if graph.degree(n)<degree_thresh:
+			graph.remove_node(n)
+
 nx.write_gexf(graph,'graph_thresh%.02f%s.gexf'%(thresh,filt))
 nx.write_gml(graph,'graph_thresh%.02f%s.gml'%(thresh,filt))
 
