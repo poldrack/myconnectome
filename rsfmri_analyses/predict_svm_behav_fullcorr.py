82a9a05f4fb4025313ef067b376be917f15142f3
poldrack <poldrack@gmail.com>
2014-12-15 13:31:58 -0800

    added necessary imports

--- rsfmri_analyses/predict_svm_behav_fullcorr.py
+++ rsfmri_analyses/predict_svm_behav_fullcorr.py
@@ -3,11 +3,17 @@ use SVM to predict outcome variables based on connectivity
 
 """
 
+import sys
+sys.path.append('/Users/poldrack/code/myconnectome/utils')
+from run_classification import run_classification
+
 import numpy
 import sklearn.preprocessing
 import scipy.stats
-from run_classification import run_classification
-from load_myconnectome_data import *
+
+sys.path.append('/Users/poldrack/code/myconnectome/timeseries_analyses')
+
+import load_myconnectome_data
 
 xvar_names=['panas.positive','panas.negative','panas.fatigue','afterscan.Anxietyduringscan','afterscan.diastolic',
 'afterscan.pulse','afterscan.systolic','morning.Sleepquality','morning.Soreness','prevevening.Alcohol',
@@ -15,9 +21,9 @@ xvar_names=['panas.positive','panas.negative','panas.fatigue','afterscan.Anxiety
 'day_of_week',"email.LIWC_negemo","email.LIWC_posemo",'zeo.zq']
 
 
-corr_data,corr_subcodes=load_fullcorr_data()
+corr_data,corr_subcodes=load_myconnectome_data.load_fullcorr_data()
 
-behavdata,behav_vars,behav_dates,behav_subcodes=load_behav_data(xvars=xvar_names)
+behavdata,behav_vars,behav_dates,behav_subcodes=load_myconnectome_data.load_behav_data(xvars=xvar_names)
 # fix t_th data
 tmp=behavdata[:,14]
 tmp[tmp==1]=numpy.nan
@@ -25,7 +31,7 @@ tmp[tmp==2]=0
 tmp[tmp==4]=1
 behavdata[:,14]=tmp
 
-corr_joint,behavdata_joint,subcodes_joint=get_matching_datasets(corr_data,behavdata,corr_subcodes,behav_subcodes)    
+corr_joint,behavdata_joint,subcodes_joint=load_myconnectome_data.get_matching_datasets(corr_data,behavdata,corr_subcodes,behav_subcodes)
     
 predacc=run_classification(corr_joint,behavdata_joint,n_train_runs=20,verbose=False,type=type)        
 predacc_null=run_classification(corr_joint,behavdata_joint,n_shuffle_runs=100,shuffle=True,verbose=False,type=type)
--- rsfmri_analyses/predict_svm_behav_wincorr.py
+++ rsfmri_analyses/predict_svm_behav_wincorr.py
@@ -2,12 +2,17 @@
 use SVM to predict outcome variables based on connectivity
 
 """
+import sys
+sys.path.append('/Users/poldrack/code/myconnectome/utils')
+from run_classification import run_classification
 
 import numpy
 import sklearn.preprocessing
 import scipy.stats
-from run_classification import run_classification
-from load_myconnectome_data import *
+
+sys.path.append('/Users/poldrack/code/myconnectome/timeseries_analyses')
+
+import load_myconnectome_data
 
 xvar_names=['panas.positive','panas.negative','panas.fatigue','afterscan.Anxietyduringscan','afterscan.diastolic',
 'afterscan.pulse','afterscan.systolic','morning.Sleepquality','morning.Soreness','prevevening.Alcohol',
@@ -15,9 +20,9 @@ xvar_names=['panas.positive','panas.negative','panas.fatigue','afterscan.Anxiety
 'day_of_week',"email.LIWC_negemo","email.LIWC_posemo",'zeo.zq']
 
 
-wincorr_data,net_names,wincorr_subcodes=load_wincorr_data()
+wincorr_data,net_names,wincorr_subcodes=load_myconnectome_data.load_wincorr_data()
 
-behavdata,behav_vars,behav_dates,behav_subcodes=load_behav_data(xvars=xvar_names)
+behavdata,behav_vars,behav_dates,behav_subcodes=load_myconnectome_data.load_behav_data(xvars=xvar_names)
 # fix t_th data
 tmp=behavdata[:,14]
 tmp[tmp==1]=numpy.nan
@@ -25,7 +30,7 @@ tmp[tmp==2]=0
 tmp[tmp==4]=1
 behavdata[:,14]=tmp
 
-wincorr_joint,behavdata_joint,subcodes_joint=get_matching_datasets(wincorr_data,behavdata,wincorr_subcodes,behav_subcodes)    
+wincorr_joint,behavdata_joint,subcodes_joint=load_myconnectome_data.get_matching_datasets(wincorr_data,behavdata,wincorr_subcodes,behav_subcodes)
     
 predacc=run_classification(wincorr_joint,behavdata_joint,n_train_runs=50,verbose=False,type=type)        
 predacc_null=run_classification(wincorr_joint,behavdata_joint,n_shuffle_runs=1000,shuffle=True,verbose=False,type=type)
