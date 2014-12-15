f13aa5c856119b27a1ee27216e1375175e674d35
poldrack <poldrack@gmail.com>
2014-12-15 13:32:24 -0800

    fixed location for output

--- rsfmri_analyses/reorder_all_adjmtx_for_netviz.py
+++ rsfmri_analyses/reorder_all_adjmtx_for_netviz.py
@@ -6,10 +6,10 @@ import glob,os
 from reorder_adjmtx_by_network import *
 
 inputdir='/Users/poldrack/Dropbox/data/selftracking/rsfmri/behav_adjmtx'
-outputdir='/Users/poldrack/code/myconnectome_d3/data'
+outputdir='/Users/poldrack/code/myconnectome-d3/data'
 
 infiles=glob.glob(os.path.join(inputdir,'*txt'))
 
 for i in infiles:
-    reorder_adjmtx_by_network(i,os.path.join(outputdir,i.replace('_adjmtx.txt','')))
+    reorder_adjmtx_by_network(i,os.path.join(outputdir,os.path.basename(i).replace('_adjmtx.txt','')))
     
