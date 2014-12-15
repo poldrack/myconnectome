ace5d19bb79ca40068ac4a426da3385d57cfe797
poldrack <poldrack@gmail.com>
2014-12-15 13:30:52 -0800

    changed to use nofix data

--- rnaseq_analyses/mk_common_solar_files.py
+++ rnaseq_analyses/mk_common_solar_files.py
@@ -1,4 +1,5 @@
 import numpy
+import os
 
 exprdata=numpy.loadtxt('/Users/poldrack/Dropbox/data/selftracking/rna-seq/WGCNA/cluster_eigengenes_GOBS.txt')
 exprdata=exprdata[:,1:]
@@ -11,7 +12,7 @@ fd_demog=numpy.loadtxt('/Users/poldrack/Dropbox/data/connectome-genome/transcrip
 
 wincorr=numpy.loadtxt('/Users/poldrack/Dropbox/data/connectome-genome/transcripts/wincorr_common.txt')
 
-f=open('/Users/poldrack/Dropbox/data/selftracking/rna-seq/WGCNA/GOBS_wincorr_expression_solar.txt','w')
+f=open('/Users/poldrack/Dropbox/data/selftracking/rna-seq/WGCNA/GOBS_nofix_wincorr_expression_solar.txt','w')
 wincorr_names=['WC%d'%i for i in range(1,wincorr.shape[1]+1)]
 me_names=['ME%d'%int(i+1) for i in range(exprdata.shape[0])]
 nuisance_names=['fd','age','sex']
@@ -21,7 +22,8 @@ header=['ID']+nuisance_names+wincorr_names+me_names
 f.write('%s\n'%','.join(header))
 
 for i in range(len(subcodes)):
-    data=[subcodes[i]] + ['%f'%x for x in fd_demog[i,:]] +  ['%f'%x for x in wincorr[i,:]] +  ['%f'%x for x in exprdata[:,i]]
+    wincorr=numpy.loadtxt(os.path.join('/Users/poldrack/Dropbox/data/connectome-genome/winmod_corr_data','%s.txt'%subcodes[i]))
+    data=[subcodes[i]] + ['%f'%x for x in fd_demog[i,:]] +  ['%f'%x for x in wincorr] +  ['%f'%x for x in exprdata[:,i]]
     f.write('%s\n'%','.join(data))
 
 f.close()
