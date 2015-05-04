e86eea68f73960344bf547d90bdc4da0122b24fe
Russ Poldrack <poldrack@gmail.com>
2015-05-04 18:19:44 -0500

    updated to new parcels

--- rsfmri_analyses/rsfmri_extract_module_summary.py
+++ rsfmri_analyses/rsfmri_extract_module_summary.py
@@ -1,7 +1,7 @@
 
 
 import os,sys
-from alff import alff
+#from alff import alff
 import numpy
 import ctypes
 _old_rtld=sys.getdlopenflags()
@@ -21,13 +21,24 @@ def z_to_r(z):
     # inverse transform
     return (numpy.exp(2.0*z) - 1)/(numpy.exp(2.0*z) + 1)
 
-
-network_names=['1 Default','2 Second Visual','3 Frontal-Parietal','4.5 First Visual (V1+)','5 First Dorsal Attention','6 Second Dorsal Attention','7 Ventral Attention/Language','8 Salience','9 Cingulo-opercular','10 Somatomotor','11.5 Frontal-Parietal Other','15 Parietal Episodic Retrieval','16 Parieto-Occipital']
+f=open('/corral-repl/utexas/poldracklab/data/selftracking/parcellation/module_names.txt')
+network_names=[]
+for l in f.readlines():
+    l_s=l.strip().split('\t')
+    network_names.append(' '.join(l_s))
+f.close()
+network_names=network_names[2:]
+nmods=len(network_names)
 
 datadir='/corral-repl/utexas/poldracklab/data/selftracking/combined_data_scrubbed'
+outdir='/corral-repl/utexas/poldracklab/data/selftracking/analyses/rsfmri_analyses'
 outdir_bwmod='/corral-repl/utexas/poldracklab/data/selftracking/analyses/rsfmri_analyses/bwmod_corr_data'
+if not os.path.exists(outdir_bwmod):
+    os.mkdir(outdir_bwmod)
 outdir_winmod='/corral-repl/utexas/poldracklab/data/selftracking/analyses/rsfmri_analyses/winmod_corr_data'
-outdir_alff='/corral-repl/utexas/poldracklab/data/selftracking/analyses/rsfmri_analyses/module_alff'
+if not os.path.exists(outdir_winmod):
+    os.mkdir(outdir_winmod)
+
 
 subcodes=[i.strip() for i in open('/corral-repl/utexas/poldracklab/data/selftracking/analyses/rsfmri_analyses/subcodes.txt').readlines()]
 
@@ -47,17 +58,16 @@ modulenum=numpy.array(modulenum)
 modules=numpy.unique(modulenum)
 modules=modules[modules>0]
 
+nparcels=616
+
 #subcodes=[subcodes[0]]
 for subcode in subcodes:
     datafile=os.path.join(datadir,subcode+'.txt')
     print datafile
-    datafile_unscrubbed=os.path.join(datadir.replace('_scrubbed',''),subcode+'.txt')
     assert os.path.exists(datafile)
 
     data=numpy.loadtxt(datafile)
-    data=data[:,:620]
-    data_unscrubbed=numpy.loadtxt(datafile_unscrubbed)
-    data_unscrubbed=data_unscrubbed[:,:620]
+    data=data[:,:nparcels]
     
     modctr=0
     modrois={}
@@ -69,7 +79,6 @@ for subcode in subcodes:
     for m in modules:
         modrois[m]=numpy.where(modulenum==m)[0]
         moddata[m]=data[:,modrois[m]]
-        moddata_unscrubbed[m]=data_unscrubbed[:,modrois[m]]
 
     for m in modules:
         modcorr=numpy.corrcoef(moddata[m].T)
@@ -87,13 +96,8 @@ for subcode in subcodes:
         modctr+=1
 
 
-    mcbw_utr=modmeancorr_between[numpy.triu_indices(13,1)]
+    mcbw_utr=modmeancorr_between[numpy.triu_indices(nmods,1)]
     numpy.savetxt(os.path.join(outdir_winmod,subcode+'.txt'),modmeancorr_within)
     numpy.savetxt(os.path.join(outdir_bwmod,subcode+'.txt'),mcbw_utr)
 
 
-f=open('bwmod_corr_labels.txt','w')
-utr=numpy.triu_indices(modeig_corr.shape[0],1)
-for i in range(utr[0].shape[0]):
-    f.write('%s\t%s\n'%(network_names[utr[0][i]],network_names[utr[1][i]]))
-f.close()
