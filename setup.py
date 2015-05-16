80169945b4330648650f7123b4941a981b23e376
Russ Poldrack <poldrack@gmail.com>
2015-05-16 13:42:10 -0500

    new addition

---
+++ setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+#
+# Copyright (C) 2013-2015 Russell Poldrack <poldrack@stanford.edu>
+
+descr = """myconnectome: a package to implement analyses for the myconnectome project"""
+
+import os
+from setuptools import setup
+
+DISTNAME="myconnectome"
+DESCRIPTION=descr
+MAINTAINER='Russ Poldrack'
+MAINTAINER_EMAIL='poldrack@stanford.edu'
+LICENSE='MIT'
+URL='http://myconnectome.org'
+DOWNLOAD_URL='https://github.com/poldrack/myconnectome/'
+VERSION='0.9.0.dev'
+
+def check_dependencies
