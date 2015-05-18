688a4420bd0d9fc55f63cc9bc22f8a3bd8bdc8eb
poldrack <poldrack@gmail.com>
2015-05-18 16:56:39 -0700

    adding dep testS

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 #
 # Copyright (C) 2013-2015 Russell Poldrack <poldrack@stanford.edu>
+# some portions borrowed from https://github.com/mwaskom/lyman/blob/master/setup.py
+
 
 descr = """myconnectome: a package to implement analyses for the myconnectome project"""
 
@@ -16,4 +18,62 @@ URL='http://myconnectome.org'
 DOWNLOAD_URL='https://github.com/poldrack/myconnectome/'
 VERSION='0.9.0.dev'
 
-def check_dependencies
+def check_dependencies():
+
+    # Just make sure dependencies exist, I haven't rigorously
+    # tested what the minimal versions that will work are
+    needed_deps = ["IPython", "numpy", "scipy", "matplotlib",
+                   "sklearn", "statsmodels", 
+                   "gtf_to_genes","pandas",
+		   "nibabel","boto"]
+    missing_deps = []
+    for dep in needed_deps:
+        try:
+            __import__(dep)
+        except ImportError:
+            missing_deps.append(dep)
+
+    if missing_deps:
+        missing = (", ".join(missing_deps)
+                   .replace("sklearn", "scikit-learn"))
+        raise ImportError("Missing dependencies: %s" % missing)
+
+if __name__ == "__main__":
+
+    if os.path.exists('MANIFEST'):
+        os.remove('MANIFEST')
+
+    import sys
+    if not (len(sys.argv) >= 2 and ('--help' in sys.argv[1:] or
+            sys.argv[1] in ('--help-commands',
+                            '--version',
+                            'egg_info',
+                            'clean'))):
+        check_dependencies()
+
+    setup(name=DISTNAME,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        description=DESCRIPTION,
+        license=LICENSE,
+        version=VERSION,
+        url=URL,
+        download_url=DOWNLOAD_URL,
+        #install_requires=["moss>=0.3.3"],
+        packages=['myconnectome', 'myconnectome.tests',
+                  'myconnectome.diffusion', 'myconnectome.rsfmri',
+                  'myconnectome.utils', 'myconnectome.taskfmri',
+                  'myconnectome.timeseries','myconnectome.rnaseq',
+                  'myconnectome.metabolomics'],
+#        scripts=['scripts/run_fmri.py', 'scripts/run_group.py',
+#                 'scripts/run_warp.py', 'scripts/setup_project.py',
+#                 'scripts/make_masks.py', 'scripts/anatomy_snapshots.py',
+#                 'scripts/surface_snapshots.py'],
+        classifiers=[
+                     'Intended Audience :: Science/Research',
+                     'Programming Language :: Python :: 2.7',
+                     'License :: OSI Approved :: BSD License',
+                     'Operating System :: POSIX',
+                     'Operating System :: Unix',
+                     'Operating System :: MacOS'],
+    )
