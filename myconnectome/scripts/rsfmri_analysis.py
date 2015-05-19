5e13c58db363511085a6b9da8e9e341507806546
Russ Poldrack <poldrack@gmail.com>
2015-05-19 15:38:42 -0500

    added detecetion of matlab

--- myconnectome/scripts/rsfmri_analysis.py
+++ myconnectome/scripts/rsfmri_analysis.py
@@ -42,12 +42,18 @@ if not os.path.exists(os.path.join(basedir,'rsfmri/module_within_corr.txt')):
 if not os.path.exists(os.path.join(basedir,'rsfmri/corrdata.npy')):
     get_corrdata.get_corrdata()
 
+
 if not os.path.exists(os.path.join(basedir,'rsfmri/geff_pos.txt')):
+  if len(run_shell_cmd('which matlab'))>0:
     cmd='matlab -nodesktop -nosplash <%s/bct_analyses.m'%filepath
     print cmd
     print 'this may take a little while...'
     run_shell_cmd(cmd)
-
+  else:
+      print 'MATLAB not available, downloading BCT results from S3'
+      get_file_from_s3('ds031/rsfmri/modularity_weighted_louvain_bct.txt',os.path.join(basedir,'rsfmri/modularity_weighted_louvain_bct.txt'))  
+      get_file_from_s3('ds031/rsfmri/PIpos_weighted_louvain_bct.txt',os.path.join(basedir,'rsfmri/PIpos_weighted_louvain_bct.txt'))  
+      get_file_from_s3('ds031/rsfmri/geff_pos.txt',os.path.join(basedir,'rsfmri/geff_pos.txt'))  
     
 if not os.path.exists(os.path.join(basedir,'parcellation/all_selected_L_new_parcel_renumbered_boundaries.func.gii')):
     mk_parcellation_boundaries.mk_parcellation_boundaries()
