121318bb8b056654f0cd1e6be7f36fa902e94cd1
Russ Poldrack <poldrack@gmail.com>
2015-05-19 14:39:09 -0500

    added module

--- myconnectome/rnaseq/__init__.py
+++ myconnectome/rnaseq/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['regress_rin_pca','get_ImmPort_eigengenes','get_WGCNA_DAVID_annotation','compare_snyderome_myconnectome']
+__all__ = ['regress_rin_pca','get_ImmPort_eigengenes','get_WGCNA_DAVID_annotation','compare_snyderome_myconnectome','get_module_descriptions']]
