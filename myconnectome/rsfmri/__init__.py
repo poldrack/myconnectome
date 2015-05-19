26760439c7c14c2d6d4a016fd023adb50173bff0
poldrack <poldrack@gmail.com>
2015-05-18 17:08:55 -0700

    removed old module

--- myconnectome/rsfmri/__init__.py
+++ myconnectome/rsfmri/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['get_rsfmri_data','mk_renumbered_parcel_file','extract_module_assignments','extract_module_summary','collapse_module_data','get_corrdata','mk_participation_index_giftis','mk_parcellation_boundaries']
+__all__ = ['mk_renumbered_parcel_file','extract_module_assignments','extract_module_summary','collapse_module_data','get_corrdata','mk_participation_index_giftis','mk_parcellation_boundaries']
