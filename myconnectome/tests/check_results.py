0f536e72a23bf9cc39b0e6eaf369f403d567fcd9
poldrack <poldrack@gmail.com>
2015-05-21 16:38:12 -0700

    working version

--- myconnectome/tests/check_results.py
+++ myconnectome/tests/check_results.py
@@ -5,10 +5,12 @@ compare results from myconnectome analysis to saved results on S3
 import numpy
 import os
 import urllib
-from myconnectome.utils.load_dataframe import load_R_dataframe
+from myconnectome.utils.load_dataframe import load_R_dataframe,load_wgcna_module_assignments
 
 basedir=os.environ['MYCONNECTOME_DIR']
 rtol=atol=1e-08
+
+print 'checking local results against cached data on S3'
 
 files_to_compare={}
 rsfmri_files=['modularity_weighted_louvain_bct.txt','PIpos_weighted_louvain_bct.txt',
@@ -17,9 +19,7 @@ rsfmri_files=['modularity_weighted_louvain_bct.txt','PIpos_weighted_louvain_bct.
 for f  in rsfmri_files:
     files_to_compare['rsfmri/'+f]='rsfmri/'+f
 
-rnaseq_files=['varstab_data_prefiltered_rin_3PC_regressed.txt',
-              'WGCNA/module_assignments_thr8_prefilt_rinPCreg.txt',
-              'WGCNA/MEs-thr8-prefilt-rinPCreg-48sess.txt']
+rnaseq_files=['varstab_data_prefiltered_rin_3PC_regressed.txt']
 for f  in rnaseq_files:
     files_to_compare['rna-seq/'+f]='RNA-seq/'+f
 
@@ -48,3 +48,25 @@ for f in files_to_compare.iterkeys():
             print 'FAIL:',f,'maxdiff =',maxdiff
     except:
         print 'FAIL:',f,'data shapes differ', repos_data.shape,local_data.shape
+
+# the following have to be set up separately because these file formats are funky
+
+modassn_local=load_wgcna_module_assignments(os.path.join(basedir,'rna-seq/WGCNA/module_assignments_thr8_prefilt_rinPCreg.txt'))
+modassn_repos=load_wgcna_module_assignments('https://s3.amazonaws.com/openfmri/ds031/'+'RNA-seq/module_assignments_thr8_prefilt_rinPCreg.txt')
+if numpy.allclose(modassn_local[0],modassn_repos[0],rtol,atol):
+    print 'PASS: rna-seq/WGCNA/module_assignments_thr8_prefilt_rinPCreg.txt'
+else:
+    maxdiff=numpy.max(modassn_local[0] - modassn_repos[0])
+    print 'FAIL: rna-seq/WGCNA/module_assignments_thr8_prefilt_rinPCreg.txt','maxdiff =',maxdiff
+
+url='https://s3.amazonaws.com/openfmri/ds031/RNA-seq/MEs-thr8-prefilt-rinPCreg-48sess.txt'
+raw_data = urllib.urlopen(url)
+f='rna-seq/WGCNA/MEs-thr8-prefilt-rinPCreg-48sess.txt'
+repos_data=numpy.loadtxt(raw_data,skiprows=1)
+local_data=numpy.loadtxt(os.path.join(basedir,f),skiprows=1)
+
+if numpy.allclose(repos_data,local_data,rtol,atol):
+    print 'PASS:',f
+else:
+    maxdiff=numpy.max(repos_data - local_data)
+    print 'FAIL:',f,'maxdiff =',maxdiff
