32fddae60a9418125b6b9a8303644fed4793c0d0
poldrack <poldrack@gmail.com>
2015-06-02 12:57:38 -0700

    added main test

--- myconnectome/taskfmri/encoding_model.py
+++ myconnectome/taskfmri/encoding_model.py
@@ -66,71 +66,72 @@ def get_design_matrix(coding,taskcodes):
     desmtx=numpy.array(desmtx)
     return desmtx
 
-try:
-    beta_hat=numpy.load('/Users/poldrack/data/selftracking/task_encoding_model/encoding_beta.npy')
-    names=['face',
- 'place',
- 'manmade_objects',
- 'words',
- 'characters',
- 'body_parts',
- 'novel_characters',
- 'response',
- 'responsetime',
- 'motion',
- 'number',
- 'maintenance',
- 'manipulation',
- 'inhibition',
- 'error_detection']
- 
-except:
-    coding,names=get_codes()
-
-    files,taskcodes=get_files(coding)
-
-    contrastdata=load_data(files)
-
-    desmtx=get_design_matrix(coding,taskcodes)
-    desmtx=desmtx-numpy.mean(desmtx,0)
-    df = desmtx.shape[0] - desmtx.shape[1]
+if __name__=="__main__":
+    try:
+        beta_hat=numpy.load('/Users/poldrack/data/selftracking/task_encoding_model/encoding_beta.npy')
+        names=['face',
+     'place',
+     'manmade_objects',
+     'words',
+     'characters',
+     'body_parts',
+     'novel_characters',
+     'response',
+     'responsetime',
+     'motion',
+     'number',
+     'maintenance',
+     'manipulation',
+     'inhibition',
+     'error_detection']
+     
+    except:
+        coding,names=get_codes()
     
-    tstat=numpy.zeros((desmtx.shape[1],32492*2))
-    lrand_scores=numpy.zeros((desmtx.shape[1],32492*2))
-    betahat=numpy.zeros((desmtx.shape[1],32492*2))
-    badctr=0
-    lrand=sklearn.linear_model.RandomizedLasso(n_jobs=11)
-    lr=sklearn.linear_model.Lasso(alpha=0.01)
-    ctr=0
-    ctr2=0
-    for i in range(contrastdata.shape[1]):
-        if ctr==100:
-            ctr=0
-            ctr2+=1
-            print ctr2
-        else:
-            ctr+=1
-            ctr2+=1
-        y=contrastdata[:,i]-numpy.mean(contrastdata[:,i])
-        lrand.fit(desmtx,y)
-        lr.fit(desmtx,y)
-        resid=y-desmtx.dot(lr.coef_)
-        sse=numpy.dot(resid,resid)/float(df)
-        tstat[:,i]=lr.coef_/sse
-        betahat[:,i]=lr.coef_
-        lrand_scores[:,i]=lrand.scores_
-
-
-tstat[numpy.isnan(tstat)]=0
-
-lh=nibabel.gifti.GiftiImage()
-rh=nibabel.gifti.GiftiImage()
-nvert=32492
-
-numpy.save(os.path.join(basedir,'analyses/task_analyses/encoding_tstat_lasso.npy'),tstat)
-numpy.save(os.path.join(basedir,'analyses/task_analyses/encoding_rand_lasso.npy'),lrand_scores)
-
-array_to_gifti_32k(tstat,os.path.join(basedir,'analyses/task_analyses/encoding_tstat_lasso'),names)
-array_to_gifti_32k(lrand_scores,os.path.join(basedir,'analyses/task_analyses/encoding_randlasso_scores'),names)
-
-
+        files,taskcodes=get_files(coding)
+    
+        contrastdata=load_data(files)
+    
+        desmtx=get_design_matrix(coding,taskcodes)
+        desmtx=desmtx-numpy.mean(desmtx,0)
+        df = desmtx.shape[0] - desmtx.shape[1]
+        
+        tstat=numpy.zeros((desmtx.shape[1],32492*2))
+        lrand_scores=numpy.zeros((desmtx.shape[1],32492*2))
+        betahat=numpy.zeros((desmtx.shape[1],32492*2))
+        badctr=0
+        lrand=sklearn.linear_model.RandomizedLasso(n_jobs=11)
+        lr=sklearn.linear_model.Lasso(alpha=0.01)
+        ctr=0
+        ctr2=0
+        for i in range(contrastdata.shape[1]):
+            if ctr==100:
+                ctr=0
+                ctr2+=1
+                print ctr2
+            else:
+                ctr+=1
+                ctr2+=1
+            y=contrastdata[:,i]-numpy.mean(contrastdata[:,i])
+            lrand.fit(desmtx,y)
+            lr.fit(desmtx,y)
+            resid=y-desmtx.dot(lr.coef_)
+            sse=numpy.dot(resid,resid)/float(df)
+            tstat[:,i]=lr.coef_/sse
+            betahat[:,i]=lr.coef_
+            lrand_scores[:,i]=lrand.scores_
+    
+    
+    tstat[numpy.isnan(tstat)]=0
+    
+    lh=nibabel.gifti.GiftiImage()
+    rh=nibabel.gifti.GiftiImage()
+    nvert=32492
+    
+    numpy.save(os.path.join(basedir,'analyses/task_analyses/encoding_tstat_lasso.npy'),tstat)
+    numpy.save(os.path.join(basedir,'analyses/task_analyses/encoding_rand_lasso.npy'),lrand_scores)
+    
+    array_to_gifti_32k(tstat,os.path.join(basedir,'analyses/task_analyses/encoding_tstat_lasso'),names)
+    array_to_gifti_32k(lrand_scores,os.path.join(basedir,'analyses/task_analyses/encoding_randlasso_scores'),names)
+    
+    
