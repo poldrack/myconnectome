8ad6240fc9925c2f23a542e5e6112494cf788ceb
poldrack <poldrack@gmail.com>
2015-06-07 16:36:41 -0700

    updated log location

--- myconnectome/utils/send_log.py
+++ myconnectome/utils/send_log.py
@@ -14,13 +14,13 @@ def send_log(type=None):
     
     ts = time.time()
     
-    bucket_name = 'myconnectome-log'
+    bucket_name = 'myconnectome'
     
     conn = S3Connection(anon=True)
     bucket = conn.get_bucket(bucket_name)
     
     mac = get_mac()
     
-    k = bucket.new_key('%s:%d:%d'%(type,mac,ts))
+    k = bucket.new_key('logs/%s:%d:%d'%(type,mac,ts))
     k.set_contents_from_string(' '.join(platform.uname()))
     
