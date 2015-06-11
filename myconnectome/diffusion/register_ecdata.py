98e949437618c0ff8fc757fc1cbb9bc51a7adf5e
Russ Poldrack <poldrack@gmail.com>
2015-06-11 17:56:23 -0500

    about to change to combined data

--- myconnectome/diffusion/register_ecdata.py
+++ myconnectome/diffusion/register_ecdata.py
@@ -1,5 +1,8 @@
 """
-run bedpostx on eddy-corrected data
+make low-b images
+register them to T1
+make inverse warps
+warp parcellation to dti space for each 
 
 
 """
@@ -10,13 +13,23 @@ import numpy
 
 import nipype.interfaces.fsl as fsl
 import nibabel as nib
-from nipype.workflows.dmri.fsl.dti import create_bedpostx_pipeline
 
 
-basedir=os.path.abspath('/scratch/projects/UT/poldracklab/poldrack/selftracking/MRI/')
-codedir='/scratch/projects/UT/poldracklab/poldrack/selftracking/myconnectome/myconnectome/diffusion'
 
-f=open(os.path.join(codedir,'run_all_individual_bedpost.sh'),'w')
+basedir=os.path.abspath('/corral-repl/utexas/poldracklab/data/selftracking')
+
+
+fsdir='/corral-repl/utexas/poldracklab/data/selftracking/FREESURFER_fs_LR/7112b_fs_LR'
+anatfile=os.path.join(fsdir,'sub013_mpr1_on_TRIO_Y_NDC_111.nii.gz')
+anatfile_brain=os.path.join(fsdir,'sub013_mpr1_on_TRIO_Y_NDC_111_brain.nii.gz')
+wmsegfile=anatfile_brain.replace('.nii.gz','_wmseg.nii.gz')
+
+if not os.path.exists(wmsegfile):
+    print 'segmenting anat file'
+    fast=fsl.FAST()
+    fast.inputs.in_files=anatfile_brain
+    fast.run()
+    os.rename(anatfile_brain.replace('.nii.gz','_pve_2.nii.gz'),anatfile_brain.replace('.nii.gz','_wmseg.nii.gz'))
 
 datafiles=glob.glob(os.path.join(basedir,'stanford_diffusion/9697/*ec.nii.gz'))
 
@@ -25,6 +38,7 @@ for ecdata in datafiles:
     mean_eclowb=ecdata.replace('_ec.nii.gz','_ec_lowb.nii.gz')
 
     if not os.path.exists(mean_eclowb):
+        print 'making mean lowb'
         ecimg=nib.load(ecdata)
         eddy_data=ecimg.get_data()
         bvals=numpy.loadtxt(ecdata.replace('.nii.gz','.bval'))
@@ -47,16 +61,16 @@ for ecdata in datafiles:
     else:
         print 'using existing mean_lowb_brain'
 
+    # register to 1mm anatomy
+
+    flirt=fsl.FLIRT()
+    flirt.inputs.in_file=mean_eclowb
+    flirt.inputs.out_matrix_file=mean_eclowb.replace(".nii.gz","_reg2t1.mat")
+    flirt.inputs.reference=anatfile_brain
+    flirt.inputs.wm_seg=anatfile_brain.replace('.nii.gz','_wmseg.nii.gz')
+    flirt.inputs.cost_func='bbr'
+    flirt.inputs.verbose=9
+    flirt.inputs.out_file=mean_eclowb.replace(".nii.gz","_reg2t1.nii.gz")
+    if not os.path.exists(flirt.inputs.out_matrix_file):
+        flirt.run()
         
-    
-
-if 1:
-    bpx=create_bedpostx_pipeline("nipype_bedpostx")
-
-    bpx.inputs.inputnode.dwi=ecdata
-    bpx.inputs.inputnode.mask=mean_eclowb.replace('_brain','_brain_mask')
-    bpx.inputs.inputnode.bvecs=ecdata.replace('.nii.gz','.bvec')
-    bpx.inputs.inputnode.bvals=ecdata.replace('.nii.gz','.bval')
-    bpx.inputs.xfibres.n_fibres=2
-    bpx.inputs.xfibres.use_gpu=True
-
