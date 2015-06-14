c08ae82904d9eccd0fd754e3542bdf6c15fdc9aa
Russ Poldrack <poldrack@gmail.com>
2015-06-14 14:27:31 -0500

    multiple sets

--- myconnectome/diffusion/run_all_probtrackx.py
+++ myconnectome/diffusion/run_all_probtrackx.py
@@ -2,43 +2,55 @@
 run probtrackx from each ROI to all others
 """
 
-import os
+import os,glob
 
 distcorr=''  # 'no' or set to '' for distcorr
 
-outdir='/scratch/projects/UT/poldracklab/poldrack/selftracking/MRI/DTI/probtrackx_outputs_%sdistcorr_term'%distcorr
-pathdir='/scratch/projects/UT/poldracklab/poldrack/selftracking/MRI/DTI/tracking_pathfiles'
-termdir='/scratch/projects/UT/poldracklab/poldrack/selftracking/MRI/DTI/termination_masks'
-bpxdir='/scratch/projects/UT/poldracklab/poldrack/selftracking/MRI/DTI/merged.bedpostX'
-roidir='/scratch/projects/UT/poldracklab/poldrack/selftracking/MRI/DTI/parcels_dtispace'
-nodif='/scratch/projects/UT/poldracklab/poldrack/selftracking/MRI/DTI/merged/nodif_brain_mask.nii.gz'
+basedir='/scratch/projects/UT/poldracklab/poldrack/selftracking/MRI/stanford_diffusion/combined_eddy_corrected'
+outdir=os.path.join(basedir,'probtrackx_outputs_%sdistcorr_term'%distcorr)
+if not os.path.exists(outdir):
+  os.mkdir(outdir)
+                    
+pathdir=os.path.join(basedir,'tracking_pathfiles')
+if not os.path.exists(pathdir):
+  os.mkdir(pathdir)
 
-f=open("roipaths.txt")
-roipaths=f.readlines()
-f.close()
+termdir=os.path.join(basedir,'termination_masks')
+
+bpxdir=os.path.join(basedir,'bedpostx_combined.bedpostX')
+
+roidir=os.path.join(basedir,'seed_masks')
+
+nodif=os.path.join(basedir,'bedpostx_combined/nodif_brain_mask.nii.gz')
+
+
+roipaths=glob.glob(os.path.join(roidir,'parce*'))
+roipaths.sort()
+
+                   
 
 # make target mask files
-if 0:
-  for i in range(len(roipaths)):
+
+for i in range(len(roipaths)):
     roinum=i+1
     f=open(os.path.join(pathdir,'paths_roi%03d'%roinum),'w')
     for j in range(len(roipaths)):
         if not i==j:
-            f.write(roipaths[j])
+            f.write(roipaths[j]+'\n')
     f.close()
 
-  
-f=open('run_all_ptx_%sdistcorr_term.sh'%distcorr,'w')
+
+f1=open('run_all_ptx_%sdistcorr_term_set1.sh'%distcorr,'w')
+f2=open('run_all_ptx_%sdistcorr_term_set2.sh'%distcorr,'w')
+
 for i in range(len(roipaths)):
-    if i<310:
-        hemis='L'
-    elif i>619:
-        hemis='S'
-    else:
-        hemis='R'
-    
-    cmd='probtrackx2 -s %s/merged -m %s -x %s/parcels.%s.bin.clean.%03d.nii.gz -o roi%03d --opd --os2t --dir=%s/probtrackx_%sdistcorr_roi%03d --targetmasks=%s/paths_roi%03d --waypoints=wm_dtimask.nii.gz --avoid=csf_dtimask.nii.gz -P 20000  --stop=%s/roi%03d.nii.gz'%(bpxdir,nodif,roidir,hemis,i+1,i+1,outdir,distcorr,i+1,pathdir,i+1,termdir,i+1)  # --stop=%s/roi%03d.nii.gz 
-    if distcorr=='':
+
+  cmd='probtrackx2 -s %s/merged -m %s -x %s/parcel%03d.nii.gz -o roi%03d --opd --os2t --dir=%s/probtrackx_%sdistcorr_roi%03d --targetmasks=%s/paths_roi%03d --waypoints=%s/wm_dtimask.nii.gz --avoid=%s/csf_dtimask.nii.gz -P 50000  --stop=%s/parcel%03d.nii.gz'%(bpxdir,nodif,roidir,i+1,i+1,outdir,distcorr,i+1,pathdir,i+1,basedir,basedir,termdir,i+1)  # --stop=%s/roi%03d.nii.gz 
+  if distcorr=='':
         cmd=cmd+' --pd'
-    f.write(cmd+'\n')
-f.close()
+        if i<(len(roipaths)/2):
+          f1.write(cmd+'\n')
+        else:
+          f2.write(cmd+'\n')
+f1.close()
+f2.close()
