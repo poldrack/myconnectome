27a3701fac77b59ad2c89ff83fea3b4e9ad9c2b4
poldrack <poldrack@gmail.com>
2015-06-22 14:49:52 -0700

    added new modules

--- myconnectome/rsfmri/__init__.py
+++ myconnectome/rsfmri/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['mk_full_network_graph','mk_renumbered_parcel_file','extract_module_assignments','extract_module_summary','collapse_module_data','get_corrdata','mk_participation_index_giftis','mk_parcellation_boundaries']
+__all__ = ['mk_connectome_figures','connectome_similarity_timeseries','mk_sorted_adjmatrices','mk_full_network_graph','mk_renumbered_parcel_file','extract_module_assignments','extract_module_summary','collapse_module_data','get_corrdata','mk_participation_index_giftis','mk_parcellation_boundaries']
