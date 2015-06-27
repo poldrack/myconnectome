711c35bd71198ea20363ec1b47b3fa45f6545d16
Russ Poldrack <poldrack@gmail.com>
2015-06-27 14:45:53 -0500

    fixed json

--- myconnectome/openfmri/merge_washu_files.py
+++ myconnectome/openfmri/merge_washu_files.py
@@ -9,28 +9,35 @@ washubase='/scratch/01329/poldrack/selftracking/washu'
 basedirs=['vc39556','vc39556_2']
 # 20150402_085939RSFC240FramesEO32Slicess025a001_238.nii.gz
 
-ecfile=open(os.path.join(washubase,'studyinfo.txt'),'w')
+outdir='/scratch/01329/poldrack/selftracking/ds031/sub00001/ses105/functional'
+
+runctr={'task001':1,'task008':1}
 
 for b in range(len(basedirs)):
     basedir=os.path.join(washubase,basedirs[b])
-    rsfiles=glob.glob(os.path.join(basedir,'*RSFC*_001.nii.gz'))
+    rsfiles=glob.glob(os.path.join(basedir,'nifti/*RSFC*001.nii.gz'))
     rsfiles.sort()
     
     for r in rsfiles:
 
         seriesnum=int(r.split('Slicess')[1].split('a')[0])
         if r.find('EO')>-1:
-            eo='open'
+            eo='task008'
+            taskname='rest - eyes open'
+            cogat='http://cognitiveatlas.org/task/id/trm_4c8a834779883'
+            
         else:
-            eo='closed'
-        print eo
-        ecfile.write('%d\t%d\t%s\n'%(b+1,seriesnum,eo))
-        cmd='fslmerge -t %s/part%d_series%03d.nii.gz %s'%(basedir,b+1,seriesnum,r.replace('_001.','_*.'))
-        if not os.path.exists('%s/part%d_series%03d.nii.gz'%(basedir,b+1,seriesnum)):
+            eo='task001'
+            taskname='rest - eyes closed'
+            cogat='http://cognitiveatlas.org/task/id/trm_54e69c642d89b'
+        #print eo
+        outfile='%s/sub00001_ses105_%s_run%03d.nii.gz'%(outdir,eo,runctr[eo])
+        cmd='cp %s %s'%(r,outfile)
+        if not os.path.exists(outfile):
             print cmd
         # get dicom header
         dcmfile=glob.glob('%s/DICOM/VC*.MR.HEAD_LAUMANN.%04d.0001.2015.04.02.*.IMA'%(basedir,seriesnum))[0]
-        print dcmfile
+        #print dcmfile
         dcmdata=dicom.read_file(dcmfile)
         dcmdict={}
         for k in dcmdata.dir():
@@ -38,17 +45,22 @@ for b in range(len(basedirs)):
 
             try:
                 dd.value.decode('ascii')
-                dcmdict[dd.name]=dd.value
+                dcmdict[dd.name.replace(' ','')]=dd.value
             except:
                 try:
                     dd.value.original_string.decode('ascii')
-                    dcmdict[dd.name]=dd.value
+                    dcmdict[dd.name.replace(' ','')]=dd.value
                 except:
                     pass
-        jsonfile='%s/part%d_series%03d.json'%(basedir,b+1,seriesnum)
+        dcmdict['TaskName']=taskname
+        dcmdict['TaskCogatlasId']=cogat
+        if dcmdict['RepetitionTime']>100:
+            dcmdict['RepetitionTime']=dcmdict['RepetitionTime']/1000.0
+        jsonfile=outfile.replace('nii.gz','json')
+        
         f=open(jsonfile,'w')
         f.write(json.dumps(dcmdict,indent=4))
         f.close()
+        runctr[eo]+=1
 
-ecfile.close()        
                                                   
