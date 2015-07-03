a15e13891688a42ea287cb98d745397607f4b3c5
poldrack <poldrack@gmail.com>
2015-07-03 09:49:07 -0700

    added new functions

--- myconnectome/utils/__init__.py
+++ myconnectome/utils/__init__.py
@@ -1,2 +1,2 @@
-__all__ = ['get_data','get_hubs','labels_to_gii','load_dataframe','netutils',
+__all__ = ['getmd5sum','download_file','get_data','get_hubs','labels_to_gii','load_dataframe','netutils',
            'run_shell_cmd','set_structure','ts_shuffle']
