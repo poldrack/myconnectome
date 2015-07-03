82dd4f78d2880f79f63da17a6265c7d29737f005
poldrack <poldrack@gmail.com>
2015-07-03 09:49:22 -0700

    using new method with basefilelist

--- myconnectome/utils/get_data.py
+++ myconnectome/utils/get_data.py
@@ -1,5 +1,5 @@
 """
-get_data.py: functions to download data for myconnectome project from Stanford archive
+get_data.py: functions to download data for myconnectome project from cloudfront archive
 Options:
     --all: get all basic data needed for analysis
     --rawfunc: get raw fMRI data
@@ -12,17 +12,22 @@ import os,getopt,sys
 import urllib
 import re
 import datetime
-import hashlib
-import requests
+from getmd5sum import getmd5sum
 from myconnectome.utils.run_shell_cmd import run_shell_cmd
+from myconnectome.utils.download_file import DownloadFile
 
 def timestamp():
     return datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
 
-   
-basedir=os.environ['MYCONNECTOME_DIR']
+dataurl='http://d2bmty58oscepi.cloudfront.net'
+basefileurl=dataurl+'/basefilelist_md5.txt'
 
-dataurl='http://web.stanford.edu/group/poldracklab/myconnectome-data'
+basedir=os.environ['MYCONNECTOME_DIR']
+basefile=os.path.join(basedir,'basefilelist_md5.txt')
+
+#dataurl='http://web.stanford.edu/group/poldracklab/myconnectome-data'
+
+
 
 def get_base_data(logfile=None,overwrite=None):
     if logfile:
@@ -33,8 +38,21 @@ def get_base_data(logfile=None,overwrite=None):
         owcmd=''
     else:
         owcmd='-N'
-    cmd='wget -N -r -l inf --no-remove-listing --no-parent -nH --cut-dirs=4 %s %s -R "index.html*","*.gif","robots.txt" -P %s %s/base/'%(owcmd,logcmd,basedir,dataurl)
-    run_shell_cmd(cmd)
+    # get base list
+    DownloadFile(basefileurl,basefile)
+    basefiles=[i.strip().split('\t') for i in open(basefile).readlines()]
+    for b in basefiles:
+        if os.path.exists(os.path.join(basedir,b[0])):
+            md5sum=getmd5sum(os.path.join(basedir,b[0]))
+            if md5sum==b[1]:
+                print 'using existing file:',b[0]
+                continue
+        print 'downloading',b[0]
+        DownloadFile(dataurl+'/'+b[0],os.path.join(basedir,b[0]))
+        try:
+            assert getmd5sum(os.path.join(basedir,b))==b[1]
+        except:
+            print 'md5sum does not match for ',b[0]
   
 def get_directory(d,logfile=None,overwrite=None):
     if logfile:
