82243868c4d3528387ab6b0bd8a92c57606826c6
poldrack <poldrack@gmail.com>
2015-07-06 11:44:24 -0700

    added handing of retries

--- myconnectome/utils/download_file.py
+++ myconnectome/utils/download_file.py
@@ -8,12 +8,23 @@ Created on Fri Jul  3 09:13:04 2015
 """
 import requests
 import os
+from requests.packages.urllib3.util import Retry
+from requests.adapters import HTTPAdapter
+from requests import Session, exceptions
 
 # from http://stackoverflow.com/questions/16694907/how-to-download-large-file-in-python-with-requests-py
 def DownloadFile(url,local_filename):
     if not os.path.exists(os.path.dirname(local_filename)):
         os.makedirs(os.path.dirname(local_filename))
-    r = requests.get(url)
+    s=requests.Session()
+    s.mount('https://',HTTPAdapter(max_retries=Retry(total=5,status_forcelist=[500])))
+    
+    connect_timeout = 1.0
+
+    r = s.get(url=url,timeout=(connect_timeout, 10.0))
+    #except requests.exceptions.ConnectTimeout:
+    #    print "Too slow Mojo!"
+        
     with open(local_filename, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024): 
             if chunk: # filter out keep-alive new chunks
